721a42680f827975eeb4c2e8de3b69c8051ca5e6
Shovon96 <fakhruddinshovon@gmail.com>
2025-05-30 23:56:39 +0600

    Asyncrhonus way file read and write system

--- fileSystem.js
+++ fileSystem.js
@@ -14,3 +14,19 @@ const readText = fs.readFileSync("./file.txt", {encoding: 'utf-8'}); // // text 
 
 console.log(readText);
 console.log('task 3');
+
+
+
+// Asyncrhonus way file read and write system.
+
+console.log('task-Async 1');
+const writeText2 = "Learning Asyncrhonusly file system read";
+fs.writeFileSync("./file.txt", writeText2);
+fs.readFile('./file.txt', {encoding: 'utf-8'}, (err, data) => {
+    if (err){
+        console.log('Something went wrong', err)
+    }
+    console.log(data, 'inside');
+});
+
+console.log('task-Async 3');
